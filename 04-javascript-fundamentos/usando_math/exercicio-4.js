e612df7a5fc5486de6ac9aa78612af7c162f1914
lucaskazuma <lucaskazuma@hotmail.com>
2023-04-24 23:23:20 -0300

    refactor: Criação de variável para uso de métodos Math.ceil e Math.floor

--- 04-javascript-fundamentos/usando_math/exercicio-4.js
+++ 04-javascript-fundamentos/usando_math/exercicio-4.js
@@ -1,2 +1,4 @@
 let random = Math.random(0,1)
+let ceil = Math.ceil(random);
+let floor = Math.floor(random);
 console.log('O numero aleatório gerado é ' + random + '. O numero aleatório arredondado para cima é ' + Math.ceil(random) + ' e arredondado para baixo é ' + Math.floor(random) + '.')
