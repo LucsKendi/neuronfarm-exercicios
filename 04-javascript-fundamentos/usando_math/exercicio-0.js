e05ab9538c73f1f263994ce22a8fc4b61ca38f4d
lucaskazuma <lucaskazuma@hotmail.com>
2023-04-24 22:55:18 -0300

    refactor: Utilização de variável para aplicação de método Math.round

--- 04-javascript-fundamentos/usando_math/exercicio-0.js
+++ 04-javascript-fundamentos/usando_math/exercicio-0.js
@@ -1,4 +1,4 @@
 let numero = 39;
 let raizQuadrada = Math.sqrt(numero);
 let round = Math.round(raizQuadrada);
-console.log(('A raiz quadrada de ' + numero + ' é ' + raizQuadrada + '. Arredondando, temos ' + Math.round(raizQuadrada) + '.'));
+console.log(('A raiz quadrada de ' + numero + ' é ' + raizQuadrada + '. Arredondando, temos ' + round + '.'));
