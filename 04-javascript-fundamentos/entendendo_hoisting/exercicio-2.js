da5d18c43f88e570cebd58a140cbfeec275c3abd
lucaskazuma <lucaskazuma@hotmail.com>
2023-04-28 14:33:55 -0300

    feat: Desenvolvendo exercício 2 de hoisting

---
+++ 04-javascript-fundamentos/entendendo_hoisting/exercicio-2.js
@@ -0,0 +1,6 @@
+console.log(hoistingLet);
+let hoistingLet = 50;
+/* 
+ao contrário do que ocorre com VAR, LET não "puxa" a variável para cima, resultando
+em erro ao tentar utilizá-la antes de sua inicialização
+*/
